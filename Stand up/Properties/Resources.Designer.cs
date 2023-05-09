5cc23204ca1bde8444949163f1ebd62b8652a1c8
Rodrigo Narciso <128933829+Rnarciso15@users.noreply.github.com>
2023-05-09 11:02:44 +0100

    verificações inserir func e cliente concluido

--- Stand up/Properties/Resources.Designer.cs
+++ Stand up/Properties/Resources.Designer.cs
@@ -553,6 +553,16 @@ namespace Stand_up.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap user {
+            get {
+                object obj = ResourceManager.GetObject("user", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap volkswagen {
             get {
                 object obj = ResourceManager.GetObject("volkswagen", resourceCulture);
