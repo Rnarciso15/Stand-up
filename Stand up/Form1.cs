a96c342bd2e3f206a8deda525a0674aa33c83e7b
Rodrigo Narciso <128933829+Rnarciso15@users.noreply.github.com>
2023-05-11 11:05:32 +0100

    delay resolvido

--- Stand up/Form1.cs
+++ Stand up/Form1.cs
@@ -70,6 +70,7 @@ namespace Stand_up
        
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.DoubleBuffered = true;
             timer1.Start();
             watch f2 = new watch();
             guna2Panel3.Controls.Clear();
